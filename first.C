9d1415c7c71d210348815524e4360263a9f6a504
Aakshant Kumar <kumaraakshant2005@gmail.com>
2025-01-06 17:00:15 +0530

    Update first.C

--- first.C
+++ first.C
@@ -2,5 +2,10 @@
 using namespace std;
 // If we dont use namespace std then we have to use std::cout and std::cin
 int main(){
-    cout<<"Hello World"<< endl;    return 0;
+    cout<<"Hello World"<< endl;
+    return 0;
 }
+// endl is used to end the line and move to next line
+// cout is used to print the output on the screen
+// namespace is used to avoid the use of std::cout and std::cin
+//cin is used to take input from the user
